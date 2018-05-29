a832413254466b55b715a14f952dabf977b54351
adi patria <adipatria66@gmail.com>
2018-05-29 09:40:43 +0700

    biodata

---
+++ biodata.php
@@ -0,0 +1,15 @@
+<?php
+$nim = "155610097";
+$nama = 'Adi Patria';
+$umur = 23;
+$alamat = 'Kalasan';
+$status = TRUE;
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama <br>";
+print "Umur : " . $umur; print "<br>";
+printf ("Alamat : $alamat<br>");
+if ($status)
+echo "Status : Aktif";
+else
+echo "Status : Tidak Aktif";
+?>
